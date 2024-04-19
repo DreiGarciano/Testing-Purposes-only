44fcc7871626550f4ea6bcfc71de472eed509bb7
DreiGarciano <152365768+DreiGarciano@users.noreply.github.com>
2024-04-19 15:44:52 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function handleClick() {
+    let thoughts = document.getElementById("box").value;
+    alert(thoughts + " miss na kita") 
+    alert(thoughts + " Hindi mo ba ako namimiss") 
+    alert(thoughts + " chat na ulit tayo plz") 
+}
